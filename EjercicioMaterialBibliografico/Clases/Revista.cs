398a4d8daf03c9810974dadd7dd25b2b3c7a07cc
Matias <Matibravoneron@gmail.com>
2025-04-07 00:08:14 -0300

    Add Revista.class

---
+++ EjercicioMaterialBibliografico/Clases/Revista.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Revista : MaterialBibliografico
+    {
+        private int volumen { get; set; }
+
+        public Revista(string titulo, string autor, int año, int volumen) :base(titulo, autor, año) 
+        {
+            this.volumen = volumen;
+        }
+
+        public override void mostrarInformacion()
+        {
+            base.mostrarInformacion();
+            Console.Write($", Volumen: {volumen}");
+            Console.WriteLine();
+        }
+    }
+}
